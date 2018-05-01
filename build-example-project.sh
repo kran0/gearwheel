de4a28c3238461579e1d91a48dd0649d66121737
Pavel SHumskii <shumskii@gmail.com>
2018-05-01 16:39:45 +0700

    fix suitable comments in examples

--- build-example-project.sh
+++ build-example-project.sh
@@ -9,22 +9,24 @@ wheelctl start dind > "$TASK_SVC_TMP"; . "$TASK_SVC_TMP" # Start DIND. Ugly? Sor
 set -ex # Enable paranoid mode. The build script starts here.
 # -----------------------------------------------------------------------------
 
-# Example tool: using dockerised 'cat' pipe to dockerised 'gzip'. Running in dind
+# First example: using dockerised 'cat' pipe to dockerised 'gzip'. Running in dind
+# Dockerd (thanks to dind: Docker in docker) is running inside a container on the host.
+# cat runs in one container, and gzip in another! Both are inside dind. MADNESS!
 cat /etc/hostname | gzip > example.gz
 
-# Example random project: using dockerised git ad docker-compose. Building in dind
-git clone https://github.com/twogg-git/docker-compose-java.git
+# Example #2 random project with docker-compose and java. Building in dind
+git clone https://github.com/twogg-git/docker-compose-java.git # dockerised git
 pushd docker-compose-java
 [ "$(/usr/sbin/getenforce)" = "Enforcing" ] && chcon -Rt svirt_sandbox_file_t ./ # selinux right context for all project
-docker-compose build
+docker-compose build # dockerised compose
 docker-compose up -d
 docker-compose down
 popd #docker-compose-java
 
-# Example random project: using dockerised git ad docker. Building in dind
+# Example #3 random project with docker. Building in dind
 git clone https://github.com/twogg-git/docker-nginx.git
 pushd docker-nginx
-docker build -t nginx:site .
+docker build -t nginx:site . # dockerised docker client
 popd #docker-nginx
 
 # -----------------------------------------------------------------------------
